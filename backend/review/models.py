a0646a6579af9723bfcec0534391b949bb8912b8
Ditke-ZH <edit.goczan@gmail.com>
2023-07-03 16:15:26 +0200

    finished the review model set-up

--- backend/review/models.py
+++ backend/review/models.py
@@ -9,11 +9,11 @@ User = get_user_model()
 
 
 class Review(models.Model):
-    text_content = models.TextField(max_length=500)
+    text_content = models.CharField(max_length=500)
     rating = models.IntegerField(default=0, validators=[MaxValueValidator(5), MinValueValidator(0)])
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=True, null=True, related_name='reviews')
     restaurant = models.ForeignKey(to=Restaurant, on_delete=models.CASCADE, related_name='restaurant_reviews')
     liked_by = models.ManyToManyField(to=User, related_name='likes', blank=True)
 
