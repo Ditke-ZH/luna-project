dffcad84781b36d883523a091f8ddc65c4ef768a
Hossam el din <hossameldin.baioumy@hotmail.com>
2023-07-04 15:55:24 +0200

    #4-#3-Resturant card & Home page

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,11 +1,90 @@
+import { useState } from "react";
 import Container from "../../components/container/Container";
+import Button from "../../components/Button/Button";
+import ResturantCard from "../../components/ResturantCard";
+import ImagePlaceHolder from "../../assets/images/resturnat-image-placeholder.jpg";
+import { Navigation, Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 import "./home.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const dummyContent = [
+  {
+    name: "Resturant Name",
+    address: "Address",
+    totalRatingNumber: 20,
+    startsNumber: 5,
+  },
+  {
+    name: "Resturant Name",
+    address: "Address",
+    totalRatingNumber: 50,
+    startsNumber: 2,
+  },
+  {
+    name: "Resturant Name",
+    address: "Address",
+    totalRatingNumber: 10,
+    startsNumber: 3,
+  },
+  {
+    name: "Resturant Name",
+    address: "Address",
+    totalRatingNumber: 30,
+    startsNumber: 1,
+  },
+];
 
 const Home = () => {
+  const [serach, setSearch] = useState("");
+  const handelSearch = e => {
+    e.preventDefault();
+  };
   return (
-    <Container>
-      <main>Home</main>
-    </Container>
+    <main>
+      <article className="home-hero-section">
+        <Container>
+          <form className="search-conatiner" onSubmit={handelSearch}>
+            <input
+              className="search-input"
+              type="text"
+              placeholder="Search..."
+              value={serach}
+              onChange={e => setSearch(e.target.value)}
+            />
+            <Button>Search</Button>
+          </form>
+        </Container>
+      </article>
+      <Container>
+        <article>
+          <h2 className="page-title">Best rated restaurants</h2>
+          <Swiper
+            className="resturant-cards-container"
+            modules={[Navigation, Autoplay]}
+            centeredSlides
+            slidesPerView={4}
+            spaceBetween={20}
+            autoplay={{ delay: 2000 }}
+            loop={true}
+          >
+            {dummyContent.map((item, idx) => (
+              <SwiperSlide key={idx} style={{ width: "fit-content" }}>
+                <ResturantCard
+                  title={item.name}
+                  address={item.address}
+                  totalRatingNumber={item.totalRatingNumber}
+                  StarsNumber={item.startsNumber}
+                  image={ImagePlaceHolder}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </article>
+      </Container>
+    </main>
   );
 };
 
