c6d0341bac5e145d2b85907201c850c59c5b85c9
Stefan Lustenberger <chrott77@gmail.com>
2023-07-04 10:32:59 +0200

    lint again

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -174,10 +174,10 @@ EMAIL_PORT = os.environ.get('EMAIL_PORT')
 
 # DOCUMENTATION
 SWAGGER_SETTINGS = {
-    'USE_SESSION_AUTH': False,  # Change settings to True to enable Django Login option
-    'LOGIN_URL': 'admin/',  # URL For Django Login
+    'USE_SESSION_AUTH': False,      # Change settings to True to enable Django Login option
+    'LOGIN_URL': 'admin/',          # URL For Django Login
     'LOGOUT_URL': 'admin/logout/',  # URL For Django Logout
-    'SECURITY_DEFINITIONS': { # Allows usage of Access token to make requests on the docs.
+    'SECURITY_DEFINITIONS': {       # Allows usage of Access token to make requests on the docs.
         'Bearer': {
             'type': 'apiKey',
             'name': 'Authorization',
