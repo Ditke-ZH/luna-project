7989268fdb9f8469d1cc60c9b6b9e36d6220a7f6
Stefan Lustenberger <chrott77@gmail.com>
2023-07-04 09:20:08 +0200

    added email stuff in settings.py

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -138,7 +138,6 @@ STATIC_URL = 'static-files/'
 STATIC_ROOT = 'static-files/' if DEBUG else '/static-files/'
 
 USE_X_FORWARDED_HOST = True
-USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 MEDIA_URL = 'media-files/'
@@ -164,3 +163,10 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ]
 }
+
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
