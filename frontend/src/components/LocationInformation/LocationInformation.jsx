9b74fa24f447264680a18ac8e866e04c40c32843
Ditke-ZH <edit.goczan@gmail.com>
2023-07-05 16:54:38 +0200

    initial commit

--- frontend/src/components/LocationInformation/LocationInformation.jsx
+++ frontend/src/components/LocationInformation/LocationInformation.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import pin from "../../assets/icons/pin.svg";
+import phone from "../../assets/icons/phone.svg";
+import web from "../../assets/icons/web.svg";
+import "./LocationInformation.css";
 
 export default function LocationInformation() {
-  return <div>Location Information</div>;
+  return (
+    <div className="LocationInformationDiv">
+      <div className="TopDiv">
+        <div>Here comes the map</div>
+      </div>
+      <div className="BottomDiv">
+        <div className="address">
+          <img src={pin} alt="Location Pin" height="26" />
+          <div>Bahnhofstrasse 106, 8001 Zürich</div>
+        </div>
+        <div className="phone">
+          <img src={phone} alt="Phone Icon" height="26" />
+          <div>+41 44 211 53 72</div>
+        </div>
+        <div className="website">
+          <img src={web} alt="Web Icon" height="26" />
+          <a href="https://laderach.com/ch-en/">laederach.com</a>
+        </div>
+      </div>
+    </div>
+  );
 }
