4219ee37a13495702c471f4ae94f4d20a001f0ca
iamlydial <lydia.lavecchia@gmail.com>
2023-07-05 17:47:26 +0200

    typo in import of import "./UserProfileRestaurants.css"; fixed

--- frontend/src/axios/axiosInstance.js
+++ frontend/src/axios/axiosInstance.js
@@ -3,10 +3,12 @@ import axios from "axios";
 const baseUrl = import.meta.env.VITE_API_BASEURL;
 const token = localStorage.getItem("accessToken");
 
+console.log(baseUrl)
+
 export const axiosMotion = axios.create({
   baseURL: baseUrl,
   headers: {
-    Authorization: `Bearer ${token}`,
+    // Authorization: `Bearer ${token}`,
     "Content-Type": "application/json",
   },
 });
--- frontend/src/pages/userProfile/UserProfile.jsx
+++ frontend/src/pages/userProfile/UserProfile.jsx
@@ -1,36 +1,36 @@
 import {axiosMotion} from "../../axios/axiosInstance.js";
-import { useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import "./userProfile.css";
 import zurichSkyline from '../../assets/images/zuerich-skyline.jpg'
 import profilePicture from '../../assets/images/profile-picture.png'
 import UserProfileMenu from "../../components/UserProfile/UserProfileMenu/UserProfileMenu.jsx";
-// import UserProfileReviews from "../../components/UserProfile/UserProfileReviews/UserProfileReviews.jsx";
 import UserProfileRestaurants from "../../components/UserProfile/UserProfileRestaurants/UserProfileRestaurants.jsx";
 import UserProfileEdit from "../../components/UserProfile/UserProfileEdit/UserProfileEdit.jsx";
 import UserProfileReviews from "../../components/UserProfile/UserProfileReviews/UserProfileReviews.jsx";
 import UserProfileComments from "../../components/UserProfile/UserProfileComments/UserProfileComments.jsx";
+
 const UserProfile = () => {
     const [user, setUser] = useState(null)
 
     const [selectedMenuItem, setSelectedMenuItem] = useState("restaurants");
 
     const handleMenuItemClick = (menu) => {
-    setSelectedMenuItem(menu);
-  };
-
-     useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const response = await axiosMotion.get('/users/');
-        console.log(response.data)
-        setUser(response.data);
-        console.log(response.data)
-      } catch (error) {
-        console.error('Error fetching user data:', error);
-      }
+        setSelectedMenuItem(menu);
     };
 
-    fetchUserData();
+    const fetchUserData = async () => {
+            try {
+                const response = await axiosMotion.get('/users/');
+                console.log(response)
+                setUser(response.data);
+                console.log(response.data)
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        };
+
+    useEffect(() => {
+        fetchUserData();
     }, []);
 
     return (
@@ -43,20 +43,20 @@ const UserProfile = () => {
                     <div className="user-img-menu-container">
                         <div className="user-image-container">
                             <img className="user-image" src={profilePicture} alt="user-profile-picture"/>
-                            <UserProfileMenu onItemClick={handleMenuItemClick} />
+                            <UserProfileMenu onItemClick={handleMenuItemClick}/>
                         </div>
                     </div>
-                     <div className="user-info-reviews">
-                         <div className="user-info">
-                             <h2 className="user-info-full-name">Laurent H.</h2>
-                             <p className="user-info-elements">Zurich, CH</p>
-                             <p className="user-info-elements">6 reviews</p>
-                             <p className="user-info-elements">210 comments</p>
-                         </div>
-                         {selectedMenuItem === "reviews" && <UserProfileReviews />}
-                         {selectedMenuItem === "restaurants" && <UserProfileRestaurants />}
-                         {selectedMenuItem === "edit" && <UserProfileEdit />}
-                         {selectedMenuItem === "comments" && <UserProfileComments />}
+                    <div className="user-info-reviews">
+                        <div className="user-info">
+                            <h2 className="user-info-full-name">Laurent H.</h2>
+                            <p className="user-info-elements">Zurich, CH</p>
+                            <p className="user-info-elements">6 reviews</p>
+                            <p className="user-info-elements">210 comments</p>
+                        </div>
+                        {selectedMenuItem === "reviews" && <UserProfileReviews/>}
+                        {selectedMenuItem === "restaurants" && <UserProfileRestaurants/>}
+                        {selectedMenuItem === "edit" && <UserProfileEdit/>}
+                        {selectedMenuItem === "comments" && <UserProfileComments/>}
                     </div>
                     <div className="about-user">
                         <div className="about-user-title"><h2>ABOUT LAURENT</h2></div>
@@ -68,13 +68,15 @@ const UserProfile = () => {
                             <h3 className="about-user-h3-headings">Luna Member Since</h3>
                             <p className="about-user-paragraphs">April, 2018</p>
                         </div>
-                         <div className="about-things-love-things">
+                        <div className="about-things-love-things">
                             <h3 className="about-user-h3-headings">Things I Love</h3>
                             <p className="about-user-paragraphs">Everything</p>
                         </div>
                         <div className="about-description-text">
                             <h3 className="about-user-h3-headings">Description</h3>
-                            <p className="about-user-paragraphs">Im professional photographer with an eye for details in every thing I do in my live. Every time a pass by a nice restaurant i have to stop and take notes</p>
+                            <p className="about-user-paragraphs">Im professional photographer with an eye for details in
+                                every thing I do in my live. Every time a pass by a nice restaurant i have to stop and
+                                take notes</p>
                         </div>
                     </div>
 
