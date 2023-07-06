3b14785918f81edea852d291a22f21966d9990e3
Hossam el din <hossameldin.baioumy@hotmail.com>
2023-07-06 13:03:20 +0200

    issue fixed for build

--- frontend/src/pages/Restaurant/index.jsx
+++ frontend/src/pages/Restaurant/index.jsx
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import "./restaurant.css";
-import RestaurantHeader from "../../components/RestaurantHeader/RestaurantHeader";
 import LocationInformation from "../../Components/LocationInformation/LocationInformation";
 import RestaurantReview from "../../components/RestaurantReview/RestaurantReview";
 import RestaurantInformation from "../../Components/RestaurantInformation/RestaurantInformation";
 import { axiosLuna } from "../../axios/axiosInstance";
-import Loader from "../../components/Loader/Loader";
 import Container from "../../components/container/Container";
 import StarRating from "../../components/StarRating/indx";
 
@@ -22,7 +20,7 @@ export default function RestaurantPage() {
       setFetchData(data);
     };
     getFetchData();
-  }, []);
+  }, [resturantId]);
   console.log(fetchData);
   const reviews = [fetchData?.restaurant_reviews];
 
