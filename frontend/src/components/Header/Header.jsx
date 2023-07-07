25bd7422b7a35292ccf8ff0af603d96040c0c92b
Stefan Lustenberger <chrott77@gmail.com>
2023-07-07 13:11:07 +0200

    redirect profile to login if not logged in added logout button

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,22 +2,30 @@ import LunaLogo from "../../assets/images/luna-logo.svg";
 import { NavLink, useNavigate } from "react-router-dom";
 
 import "./header.css";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {logout} from "../../store/slices/user.js";
+import {useEffect, useState} from "react";
 
-const MenuItems = [
-  { name: "Home", link: "/" },
-  { name: "Search", link: "/search" },
-  { name: "Profile", link: "/profile" },
-];
+
 
 const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const access_token = useSelector((state) => state.user.accessToken);
+  const [MenuItems, SetMenuItems] = useState([])
+
+  useEffect(() =>{
+     SetMenuItems([
+      { name: "Home", link: "/" },
+      { name: "Search", link: "/search" },
+      { name: "Profile", link: access_token ? "/profile" : "/login" },
+      ]);
+  }, [access_token])
 
   const handleLogout = e => {
     e.preventDefault();
     dispatch(logout());
+    localStorage.removeItem("refreshToken");
   }
 
   return (
@@ -31,24 +39,29 @@ const Header = () => {
             </li>
           ))}
         </ul>
-        <div className="header-buttons-container">
+        { access_token ?
+            <button
+              className="logout-button"
+              onClick={handleLogout}
+            >logout</button>
+          : <div className="header-buttons-container">
           <button
-            className="header-button-left"
-            onClick={() => {
-              navigate("/signup");
-            }}
+              className="header-button-left"
+              onClick={() => {
+                navigate("/signup");
+              }}
           >
             signup
           </button>
           <button
-            className="header-button-right"
-            onClick={() => {
-              navigate("/login");
-            }}
+              className="header-button-right"
+              onClick={() => {
+                navigate("/login");
+              }}
           >
             login
           </button>
-        </div>
+        </div>}
       </nav>
     </header>
   );
