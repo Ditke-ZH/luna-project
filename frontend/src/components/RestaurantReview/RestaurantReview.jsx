bb44bca1087a28da88e93cc5673330c58f7bd91c
Ditke-ZH <edit.goczan@gmail.com>
2023-07-07 12:05:48 +0200

    changed it back to working template

--- frontend/src/components/RestaurantReview/RestaurantReview.jsx
+++ frontend/src/components/RestaurantReview/RestaurantReview.jsx
@@ -8,99 +8,68 @@ import StarRating from "../StarRating";
 import { useSelector } from "react-redux";
 
 export default function RestaurantReview({ restaurantData }) {
-  const { restaurantId } = useParams();
+  const { resturantId } = useParams();
   const [fetchData, setFetchData] = useState([]);
-  const [fetchedReviewData, setFetchedReviewData] = useState([]);
-
-  const accessToken = useSelector((state) => state.user.accessToken);
+  const access_token = useSelector((state) => state.user.accessToken);
 
   useEffect(() => {
     const getFetchData = async () => {
       let config = null;
-      if (accessToken) {
+      if (access_token) {
         config = {
-          headers: { Authorization: `Bearer ${accessToken}` },
+          headers: { Authorization: `Bearer ${access_token}` },
           "Content-Type": "application/json",
         };
       }
-      try {
-        const res = await axiosLuna.get(`/restaurants/${restaurantId}`, config);
-        const data = res?.data;
-        setFetchData(data);
-      } catch (error) {
-        console.log("Error fetching restaurant data:", error);
-      }
+
+      const res = await axiosLuna.get(`/restaurants/${resturantId}`, config);
+      const data = res?.data;
+      setFetchData(data);
     };
     getFetchData();
-  }, [restaurantId, accessToken]);
+  }, [resturantId]);
   console.log(fetchData);
-
-  useEffect(() => {
-    const fetchReviewData = async () => {
-      let config = null;
-      if (accessToken) {
-        config = {
-          headers: { Authorization: `Bearer ${accessToken}` },
-          "Content-Type": "application/json",
-        };
-      }
-      try {
-        const response = await axiosLuna.get(
-          `/restaurants/${restaurantId}/reviews`,
-          config
-        );
-        const reviewData = response?.data;
-        setFetchedReviewData(reviewData);
-      } catch (error) {
-        console.log("Error fetching review data:", error);
-      }
-    };
-
-    fetchReviewData();
-  }, [restaurantId, accessToken]);
+  //const reviews = [fetchData?.restaurant_reviews];
 
   return (
     <div className="ReviewCardContainer">
-      {fetchedReviewData.map((review) => (
-        <div key={review.id} className="ReviewCard">
-          <div className="ReviewCardTopDiv">
-            <img src={profilePicture} alt="User Profile" width="68" />
-            <div className="LeftContainer">
-              <h5 className="reviewerName">{review.user}</h5>
-              <span className="reviewCount">
-                {restaurantData.review_count > 0
-                  ? `${restaurantData.review_count} Reviews in Total`
-                  : "0 Reviews in Total"}
-              </span>
-            </div>
-            <div className="rating">
-              <div className="starWrapper">
-                <StarRating
-                  StarRating={restaurantData.rating_average}
-                  totalRatingNumber={restaurantData.review_count}
-                  ExtraClasses="resturant-stars"
-                />
-              </div>
-            </div>
-            <span className="CreationDate">{review.created}</span>
-          </div>
-          <div className="ReviewCardBottomDiv">
-            <p className="ReviewText">{review.text_content}</p>
-            <div className="LikeCommentContainer">
-              <div className="ButtonContainer">
-                <div className="Like">
-                  <img src={like} alt="Like Icon" />
-                  Like {review.liked_by.length}
-                </div>
-                <div className="Comment">Comment {review.comments.length}</div>
-              </div>
-              <a href="#" className="ShowComments">
-                View all comments
-              </a>
-            </div>
+      <div className="ReviewCardTopDiv">
+        <img src={profilePicture} alt="User Profile" width="68" />
+        <div className="LeftContainer">
+          <h5 className="reviewerName">Username</h5>
+          <span className="reviewCount">
+            {restaurantData.review_count} Reviews in Total
+          </span>
+        </div>
+        <div className="rating">
+          <div className="starWrapper">
+            <StarRating
+              StarRating={fetchData.rating_average}
+              totalRatingNumber={fetchData.review_count}
+              ExtraClasses="resturant-stars"
+            />
           </div>
         </div>
-      ))}
+        <span className="CreationDate">date created</span>
+      </div>
+      <div className="ReviewCardBottomDiv">
+        <p className="ReviewText">
+          This location at the Bahnhofstrasse is quite friendly and easily
+          located across the street from the train station. The people there
+          seem to be quite good and helpful in their service.
+        </p>
+        <div className="LikeCommentContainer">
+          <div className="ButtonContainer">
+            <div className="Like">
+              <img src={like} alt="Like Icon"></img>Like 63
+            </div>
+            <div className="Comment">Comment 23</div>
+          </div>
+          <a href="#" className="ShowComments">
+            View all comments
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
